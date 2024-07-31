05398af111025e4e8d4650f71d95cc7a23c26772
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-07-31 03:20:28 +0200

    Configuration Docker
    - création du Dockerfile
    - modification config vitejs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // écoute sur toutes les interfaces réseau
+    port: 3000, // port 3000
+  },
 })
