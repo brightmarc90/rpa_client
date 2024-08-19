494e313da69e3a846daf2d6d1085513a9702f79e
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-08-19 23:28:55 +0200

    Intégration de la page, requête et logique d'affichage des données des écoles

--- src/views/schoolList/SchoolListView.jsx
+++ src/views/schoolList/SchoolListView.jsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
+import { getSchools } from "../../services/api/school";
+
 function SchoolListView() {
+  const [schoolList, setSchoolList] = useState([]);
+
+  useEffect(() => {
+    const execAsync = async () => {
+        try {
+            const response = await getSchools();
+            setSchoolList(response.data);
+        } catch (error) {
+            const message = error?.message;
+            if (message) console.log(message);
+        }
+    }
+    execAsync();
+  }, []);
+
   return (
-    <div>SchoolListView</div>
-  )
+    <div>
+      <h1>Liste de écoles</h1>
+      {schoolList && (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nom</th>
+              </tr>
+            </thead>
+            <tbody>
+                {schoolList.map((school, index) => (
+                    <tr key={index}>
+                    <td>{index+1}</td>
+                    <td>{school.name}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default SchoolListView
+export default SchoolListView;
