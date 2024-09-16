36bc4b48d6fd9c0bab940afd2b0cf89be9a74548
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-09-16 17:36:43 +0200

    Pagination de la liste des écoles
    - inclusion des paramètres skip et limit dans les requêtes
    - adaptation de la view à la nouvelle requête

--- src/services/api/school.js
+++ src/services/api/school.js
@@ -1,6 +1,9 @@
 import { publicInstance } from "./instance"
 
-export async function getSchools() {
-    const response = await publicInstance.get("/schools")
+export async function getSchools(skip, limit) {
+    let uri = ""
+    uri += skip? `skip=${skip}&` : ""
+    uri += limit? `limit=${limit}&` : ""
+    const response = await publicInstance.get(`/schools?${uri}`)
     return response
 }
