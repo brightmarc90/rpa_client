d6caea867d6f87cdba781d1c3084d5320bb00dda
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-09-17 14:01:30 +0200

    Pagination de liste des matières
    - Correction du hook et requête de récupération des matières
    - intégration du composant de pagination à la liste des matières

--- src/services/api/subject.js
+++ src/services/api/subject.js
@@ -1,6 +1,9 @@
 import { publicInstance } from "./instance"
 
-export async function getSubjects() {
-    const response = await publicInstance.get("/subjects")
+export async function getSubjects(skip, limit) {
+    let uri = ""
+    uri += skip? `skip=${skip}&` : ""
+    uri += limit? `limit=${limit}&` : ""
+    const response = await publicInstance.get(`/subjects?${uri}`)
     return response
 }
--- src/views/subjectList/SubjectListView.jsx
+++ src/views/subjectList/SubjectListView.jsx
@@ -1,24 +1,42 @@
 import { useEffect, useState } from "react";
 import { getSubjects } from "../../services/api/subject";
+import ListPagination from "../../components/listPagination/ListPagination";
 
 function SubjectListView() {
   const [subjectList, setSubjectList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [responseData, setResponseData] = useState(null)
+  const [listIndex, setListIndex] = useState(0)
+
+  const execAsync = async (skip, limit) => {
+    try {
+      setLoading(true)
+      const response = await getSubjects(skip, limit);
+      setResponseData(response.data)
+      setSubjectList(response.data.data);
+    } catch (err) {
+      setError(err?.message);
+      if (error) console.log(error);
+    } finally {
+      setLoading(false)
+    }
+  };
 
   useEffect(() => {
-    const execAsync = async () => {
-      try {
-        const response = await getSubjects();
-        setSubjectList(response.data);
-      } catch (error) {
-        const message = error?.message;
-        if (message) console.log(message);
-      }
-    };
-    execAsync();
+    execAsync(0, 10);
   }, []);
+
+  const changePage = (skip, limit) => {
+    setListIndex(skip)
+    execAsync(skip, limit)
+  }
+
   return (
     <div>
       <h1>Liste des matières</h1>
+      { loading && <p>Chargement en cours ...</p>}
+      { (subjectList.length == 0 && !loading ) && <p>Aucun résultat</p>}
       {subjectList.length > 0 && (
         <div>
           <table>
@@ -31,15 +49,15 @@ function SubjectListView() {
             <tbody>
               {subjectList.map((school, index) => (
                 <tr key={index}>
-                  <td>{index + 1}</td>
+                  <td>{listIndex + index + 1}</td>
                   <td>{school.name}</td>
                 </tr>
               ))}
             </tbody>
           </table>
+          <ListPagination count={responseData.total} limit={responseData.limit} changePage={changePage} />
         </div>
       )}
-      {subjectList.length == 0 && <p>Aucun résultat</p>}
     </div>
   );
 }
