775cb846b050d15e21979579d315693a51b6a1d1
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-09-22 16:02:22 +0200

    Pagination liste utilisateurs
    - ajout des paramètres skip et limit à la requête de récupération des utilisateurs
    - mise à jour de la vue (erreur, composant de pagination, chargement)

--- src/services/api/user.js
+++ src/services/api/user.js
@@ -1,6 +1,9 @@
 import { publicInstance } from "./instance"
 
-export async function getUsers() {
-    const response = await publicInstance.get("/users")
+export async function getUsers(skip, limit) {
+    let uri = ""
+    uri += skip? `skip=${skip}&` : ""
+    uri += limit? `limit=${limit}&` : ""
+    const response = await publicInstance.get(`/users?${uri}`)
     return response
 }
